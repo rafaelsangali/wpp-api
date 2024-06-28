292d53618dc6e99015136ca5785ebfa1e28bd1e3
rafaelsangali <rafaelsangali@outlook.com>
2024-06-28 16:08:53 -0300

    feat(docker): adding dockerfile

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-if [ "$DOCKER_ENV" = "true" ];
-then
+if [ "$DOCKER_ENV" = "true" ]; then
   echo "Enabling environment variables for Docker"
   echo "DOCKER_ENV=$DOCKER_ENV"
   echo
@@ -9,15 +8,14 @@ else
   mkdir -p ./dist/src
   cp ./src/env.yml ./dist/src
 fi
+
 echo "> removing dist"
 rm -rf ./dist
 echo
 echo "> transpiling..."
 npm run build
-
 echo
-echo "> Successfully build "
-
+echo "> Successfully build"
 echo
 echo "> Starting application..."
 echo
